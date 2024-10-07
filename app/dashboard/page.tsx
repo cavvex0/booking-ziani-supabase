7daf3e377abbbf2c786d8ec72547375d2edc23bd
CAVVEX0 <120098053+CAVVEX0@users.noreply.github.com>
2024-10-07 16:47:08 +0100

    ssd

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,9 @@
 import BookingTable from "@/components/BookingTable";
 import { getUsername } from "@/lib/getUsername";
-import { createClient } from "@/utils/supabase/server";
 
 const Dashboard = async () => {
-  const supabase = createClient();
   const username = await getUsername();
-  const { data: bookings } = await supabase
-    .from("bookings")
-    .select("*")
-    .order("date", { ascending: true });
 
-  if (!bookings) {
-    // Handle the case where bookings is null
-    return <div>No bookings found</div>;
-  }
   return (
     <div className="max-w-[90rem] mx-auto">
       <div className="py-10">
@@ -21,7 +11,7 @@ const Dashboard = async () => {
           Bon Retour <span className="capitalize">{username} 👋</span>
         </h1>
       </div>
-      <BookingTable bookings={bookings} />
+      <BookingTable />
     </div>
   );
 };
