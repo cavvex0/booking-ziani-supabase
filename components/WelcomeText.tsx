2cbed6da4cd415842ca94815ba25d93092c90b27
CAVVEX0 <120098053+CAVVEX0@users.noreply.github.com>
2024-10-11 16:54:27 +0100

    xv

--- components/WelcomeText.tsx
+++ components/WelcomeText.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const WelcomeText = ({ username }: { username: string }) => {
   return (
     <div className="lg:py-10 py-5 flex items-center justify-between">
-      <h1 className="font-bold text-[24px] lg:text-3xl font-jockey text-black/90">
+      <h1 className="font-[500] text-[24px] lg:text-3xl font-jockey text-black/90">
         Bon Retour <span className="capitalize">{username} 👋</span>
       </h1>
       {(username === "hannan" || username === "adam") && (
